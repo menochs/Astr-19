b981ff8d88bf548dc6dd45485267b44386365c8b
menochs <146883346+menochs@users.noreply.github.com>
2023-10-20 14:07:52 -0700

    Session 2 Prompt for Coding Journal 1

---
+++ session-two-prompt.py
@@ -0,0 +1,13 @@
+# sum of two floating point numbers
+x = 2.6
+y = 3.9
+print(x+y)
+print(type(x+y))
+# the difference between two integers 
+p = 20
+l = 6
+print(p-l)
+print(type(p-l))
+# the product of a floating point number and an integer
+print(x*p)
+print(type(x*p))
