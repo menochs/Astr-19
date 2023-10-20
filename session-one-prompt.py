fa1750bd5e55976d7f9873d9376b92643dcbea6a
menochs <146883346+menochs@users.noreply.github.com>
2023-10-20 11:27:08 -0700

    Session 1 prompt for Coding Journal 1.

---
+++ session-one-prompt.py
@@ -0,0 +1,8 @@
+#This is my name and preferred pronouns
+print("Hello I'm Madison Enochs (She/Her)!!")
+
+#My favorite movie
+print("My favorite movie is called Perks of Being a Wallflower, and it is based on a book")
+
+#My favorite food
+print("My favorite food is French Fries!")
